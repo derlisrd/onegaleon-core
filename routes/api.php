f074bb39b65a8cd98f4395a0e0141ef37fbd9544
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-14 14:45:50 -0300

    deploy

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,17 @@ Route::middleware('auth:api')->group(function(){
     Route::prefix('/movimientos')->group(function(){
         Route::get('/',[MovimientoController::class,'index']);
         Route::get('/{id}',[MovimientoController::class,'show']);
-        Route::post('/',[MovimientoController::class,'index']);
-        Route::put('/{id}',[MovimientoController::class,'index']);
-        Route::delete('/{id}',[MovimientoController::class,'index']);
+        Route::post('/',[MovimientoController::class,'store']);
+        Route::put('/{id}',[MovimientoController::class,'update']);
+        Route::delete('/{id}',[MovimientoController::class,'destroy']);
     });
 
     Route::prefix('/categorias')->group(function(){
         Route::get('/',[CategoryController::class,'index']);
         Route::get('/{id}',[CategoryController::class,'show']);
-        Route::post('/',[CategoryController::class,'index']);
-        Route::put('/{id}',[CategoryController::class,'index']);
-        Route::delete('/{id}',[CategoryController::class,'index']);
+        Route::post('/',[CategoryController::class,'store']);
+        Route::put('/{id}',[CategoryController::class,'update']);
+        Route::delete('/{id}',[CategoryController::class,'destroy']);
     });
 
 });
