e479b4d032c23e2eedb792ffa245c5ef1c4bda76
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-11-08 16:58:14 -0300

    formato

--- app/Models/Movimiento.php
+++ app/Models/Movimiento.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,7 @@ class Movimiento extends Model
         'valor',
         'category_id'
     ];
+    public function getFormattedCreatedAtAttribute(){
+        return Carbon::parse($this->created_at)->format('Y-m-d H:i');
+    }
 }
